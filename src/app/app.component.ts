4f1ef442c35725716e26a2f3327598a4277ea0d6
Aleix <aleix.canovas@in2.es>
2018-12-16 12:16:15 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public filesToUpload: Array<File> = [];
+  public url = '';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  handleFileInput(event: any) {
+    this.filesToUpload = <Array<File>>event.target.files;
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  submitImage() {
+    const formData: any = new FormData();
+    const files: Array<File> = this.filesToUpload;
+    formData.append('uploads[]', files[0], files[0]['name']);
+    this.http.post(`http://localhost:3000/image/add`, formData)
+      .subscribe(data => {
+        console.log(data);
+      }
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule, MatInputModule, MatSelectModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    HttpClientModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
