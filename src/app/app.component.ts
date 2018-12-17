eb0555cb6bf769091c147a391d52aef117d44203
aleix <aleix11fcb@gmail.com>
2018-12-17 16:51:03 +0100

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {RequestOptions} from "@angular/http";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
 
-  public filesToUpload: Array<File> = [];
+  // public filesToUpload: Array<File> = [];
+  public filesToUpload: FileList;
   public url = '';
 
   constructor(private http: HttpClient) {
@@ -16,22 +18,25 @@ export class AppComponent {
   }
 
   handleFileInput(event: any) {
-    this.filesToUpload = <Array<File>>event.target.files;
-    if (event.target.files && event.target.files[0]) {
-      let reader = new FileReader();
-
-      reader.onload = (event: any) => {
-        this.url = event.target.result;
-      };
-
-      reader.readAsDataURL(event.target.files[0]);
-    }
+    this.filesToUpload = event.target.files;
+    // this.filesToUpload = <Array<File>>event.target.files;
+    // if (event.target.files && event.target.files[0]) {
+    //   let reader = new FileReader();
+    //
+    //   reader.onload = (event: any) => {
+    //     this.url = event.target.result;
+    //   };
+    //
+    //   reader.readAsDataURL(event.target.files[0]);
+    //   console.log(reader);
+    // }
   }
 
   submitImage() {
-    const formData: any = new FormData();
-    const files: Array<File> = this.filesToUpload;
-    formData.append('uploads[]', files[0], files[0]['name']);
+    let file: File = this.filesToUpload[0];
+    const formData: FormData = new FormData();
+    formData.append('uploadFile', file, file.name);
+    console.log(formData, this.filesToUpload);
     this.http.post(`http://localhost:3000/image/add`, formData)
       .subscribe(data => {
         console.log(data);
